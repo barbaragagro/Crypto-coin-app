41cac8dfcc40b749d735d533b995e64f205be82c
barbaragagro <barbaragagro1@gmail.com>
2023-03-17 17:20:27 +0100

    fetching data and flatlisting it

--- App.js
+++ App.js
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
+import { SafeAreaView } from "react-native";
+import styled from "styled-components/native";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+  background-color: #041014;
+  align-items: center;
+  justify-content: center;
+`;
+
+const WhiteText = styled(Text)`
+  color: white;
+`;
+
+const CoinItem = ({
+  name,
+  market_cap_rank,
+  symbol,
+  image,
+  current_price,
+  market_cap,
+  market_cap_change_percentage_24h,
+}) => (
+  <View style={styles.coinItem}>
+    <Image
+      style={styles.image}
+      source={{
+        uri: image,
+      }}
+    />
+    <View>
+      <WhiteText>{name}</WhiteText>
+    </View>
+  </View>
+);
 
 export default function App() {
+  const [response, setResponse] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoading(false);
+          setResponse(result);
+        },
+        (error) => {
+          setIsLoading(false);
+          setError(error);
+        }
+      );
+  }, []);
+
+  const getCoinPrices = () => {
+    if (isLoading) {
+      return <ActivityIndicator size="large" />;
+    } else if (error) {
+      return <Text>{error}</Text>;
+    } else {
+      return (
+        <FlatList
+          data={response}
+          renderItem={({ item }) => (
+            <CoinItem name={item.name} image={item.image} />
+          )}
+          keyExtractor={(item) => item.id}
+        />
+      );
+    }
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeArea>
+      <View style={styles.container}>
+        <Text>Top Assets</Text>
+        {getCoinPrices()}
+        <StatusBar style="auto" />
+        {console.log(response)}
+      </View>
+    </SafeArea>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  coinItem: {
+    flexDirection: "row",
+  },
+  image: {
+    width: 50,
+    height: 50,
   },
 });
